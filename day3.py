89e99b7161faff7dfef4c9b365efb51b09641755
Kelly Gawne <kelly.d.gawne@intel.com>
2017-12-08 10:18:10 -0800

    Solved part 1 of day 3

--- day3.py
+++ day3.py
@@ -12,32 +12,34 @@ def TestCompare( case, expected, result):
 
 def CalcSteps(squareNum):
 	#generate blocks up to square number
-	n = 0
+	mSquareNum = 1
 	d = 1
-	x, y = 0
-	coord = [0,0]
+	x = 0
+	y = 0
+	#coord = [0,0]
 	foundNum = False
 
 	while not foundNum:
 
-		if n ==squareNum:
+		if mSquareNum ==squareNum:
 			break;
-		dif = d/d
-
+		dif = d//abs(d)
+		#print("d = " +str(d))
+		#print("dif = " + str (dif))
 		for _ in range(abs(d)):
-			print("Shift x by 1: " + str(coord))
-			coord.x += dif
-			++n
-			if n == squareNum:
-				foundNum = true
+			x += dif
+			#print("Shift x by 1: " + str((x,y)) + " squareNum = " + str(mSquareNum))
+			mSquareNum += 1
+			if mSquareNum == squareNum:
+				foundNum = True
 				break
 
 		if(not foundNum	):
 			for _ in range(abs(d)):
-				print("Shift y by 1: " + str(coord))
-				coord.y += dif
-				++n
-				if n == squareNum:
+				y += dif
+				#print("Shift y by 1: " + str((x,y))+ " squareNum = " + str(mSquareNum))
+				mSquareNum += 1
+				if mSquareNum == squareNum:
 					foundNum = True
 					break
 
@@ -48,17 +50,17 @@ def CalcSteps(squareNum):
 			d *= -1
 			d += 1	
 
-	print("Found coordinates: "+str(squareNum) + " is at " + str(coord))
+	print("Found coordinates: "+str(squareNum) + " is at " + str((x,y)))
 
 	#Calculate abs difference of cartesian coordinates
-	numSteps = abs(coord.x) + abs(coord.y)
+	numSteps = abs(x) + abs(y)
 	return numSteps;
 
 #Test Cases
 TestCompare( 1, 0, CalcSteps(1))
 TestCompare(12, 3, CalcSteps(12))
 TestCompare(23, 2, CalcSteps(23))
-TestCompare(1024, 31, CalcSteps(31))
+TestCompare(1024, 31, CalcSteps(1024))
 
 data = input("What square do you want steps for?")
 steps = CalcSteps(int(data))
